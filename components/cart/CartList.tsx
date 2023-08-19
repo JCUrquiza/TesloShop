7c33564a874c5a09ea2e0a60ea16411e1d484694
Juan <gurquiza8@gmail.com>
2023-08-19 14:16:15 -0500

    Show correct product into cart

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,14 +1,9 @@
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, useContext } from 'react';
 import NextLink from 'next/link';
 import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
 import { initialData } from '../../database/products';
 import { ItemCounter } from '../ui';
-
-const producstInCart = [
-    initialData.products[0],
-    initialData.products[1],
-    initialData.products[2]
-];
+import { CartContext } from '../../context';
 
 interface Props{
     editable?: boolean;
@@ -16,18 +11,20 @@ interface Props{
 
 export const CartList: FC<PropsWithChildren<Props>> = ({editable = false}) => {
 
+    const {cart} = useContext(CartContext);
+
     return (
         <>
             {
-                producstInCart.map( product => (
-                    <Grid container key={product.slug} spacing={2} sx={{mb:1}}>
+                cart.map( product => (
+                    <Grid container key={ product.slug + product.size } spacing={2} sx={{mb:1}}>
                         <Grid item xs={3}>
                             {/* TODO: Llevar a la página del producto */}
-                            <NextLink href='/product/slug' passHref legacyBehavior>
+                            <NextLink href={`/product/${ product.slug }`} passHref legacyBehavior>
                                 <Link>
                                     <CardActionArea>
                                         <CardMedia
-                                            image={`/products/${product.images[0]}`}
+                                            image={`/products/${product.images}`}
                                             component='img'
                                             sx={{ borderRadius: '5px' }}
                                         />
@@ -43,8 +40,16 @@ export const CartList: FC<PropsWithChildren<Props>> = ({editable = false}) => {
                                 {/* Conditional */}
                                 {
                                     editable
-                                    ? <ItemCounter />
-                                    : <Typography variant='h4'>3 items</Typography>
+                                    ? (
+                                        <ItemCounter 
+                                            currentValue={ product.quantity }
+                                            maxValue={ 10 }
+                                            updateQuantity={() => {}}
+                                        />
+                                    ) 
+                                    : (
+                                        <Typography variant='h5'>{ product.quantity } { product.quantity > 1 ? 'productos' : 'producto' }</Typography>
+                                    )
                                 }
                             </Box>
                         </Grid>
